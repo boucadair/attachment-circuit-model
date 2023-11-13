bc8784afbf6cc994d4e26e885bf174037f12766d
Med <mohamed.boucadair@orange.com>
2023-11-13 13:32:20 +0100

    Update yang.mk

--- yang/yang.mk
+++ yang/yang.mk
@@ -4,7 +4,7 @@ STDYANGDIR ?= tools/yang
 $(STDYANGDIR):
 	git clone --depth 10 -b main https://github.com/YangModels/yang $@
 
-OPTIONS=-f tree --tree-line-length=70
+OPTIONS=--tree-print-groupings --tree-no-expand-uses -f tree --tree-line-length=70
 ifeq ($(OS), Windows_NT)
 YANG_PATH="$(YANGDIR);$(STDYANGDIR)/standard/ietf/RFC/;$(STDYANGDIR)/standard/ieee/published/802.1/;$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules"
 else
@@ -17,7 +17,7 @@ TXT=$(patsubst $(YANGDIR)/%.yang,%-diagram.txt,$(YANG))
 .PHONY: yang-lint yang-gen-diagram yang-clean
 
 pyang-lint: $(STDYANG) $(STDYANGDIR)
-        pyang -V --ietf -f tree --tree-line-length=70 -p $(YANG_PATH) $(STDYANG)
+	pyang -V --ietf -p $(YANG_PATH) $(STDYANG)	
 
 yang-lint: $(STDYANG) $(STDYANGDIR)
 	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules -p $(STDYANGDIR)/standard/ieee/published/802.1/ $(STDYANG) -i
